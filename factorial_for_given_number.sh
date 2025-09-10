6fabb85b1e1dcac55bae67452f208691f34cf016
bharathsavadatti <bharath.savadatti447@gmail.com>
2025-09-10 16:42:31 +0530

    added factorial bash script

---
+++ factorial_for_given_number.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for i in "$@"
+do
+    num=$i
+    fact=1
+
+    while [ $num -gt 1 ]
+    do
+        fact=$((fact * num))
+        num=$((num - 1))
+    done
+
+    echo "The Factorial of $i is $fact"
+done
